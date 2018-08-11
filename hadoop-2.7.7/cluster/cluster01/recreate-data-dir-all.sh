7bbcd1c0a39e3650c35483142deff69b7e4da5a0
fsmflying <fangmingdev@163.com>
2018-08-11 16:50:04 +0800

    修改脚本：数据目录重建

--- hadoop-2.7.7/cluster/cluster01/recreate-data-dir-all.sh
+++ hadoop-2.7.7/cluster/cluster01/recreate-data-dir-all.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
 
 #清除数据目录
-
+ssh hadoop@master.hadoop "rm -rf /data/hadoop/cluster01"
 ssh hadoop@master.hadoop "mkdir -p /data/hadoop/cluster01/tmp"
 ssh hadoop@master.hadoop "mkdir -p /data/hadoop/cluster01/dfs/name"
 ssh hadoop@master.hadoop "mkdir -p /data/hadoop/cluster01/dfs/data"
 
+ssh hadoop@slave06.hadoop "rm -rf /data/hadoop/cluster01"
 ssh hadoop@slave06.hadoop "mkdir -p /data/hadoop/cluster01/tmp"
 ssh hadoop@slave06.hadoop "mkdir -p /data/hadoop/cluster01/dfs/name"
 ssh hadoop@slave06.hadoop "mkdir -p /data/hadoop/cluster01/dfs/data"
 
+ssh hadoop@slave07.hadoop "rm -rf /data/hadoop/cluster01"
 ssh hadoop@slave07.hadoop "mkdir -p /data/hadoop/cluster01/tmp"
 ssh hadoop@slave07.hadoop "mkdir -p /data/hadoop/cluster01/dfs/name"
 ssh hadoop@slave07.hadoop "mkdir -p /data/hadoop/cluster01/dfs/data"
 
+ssh hadoop@slave08.hadoop "rm -rf /data/hadoop/cluster01"
 ssh hadoop@slave08.hadoop "mkdir -p /data/hadoop/cluster01/tmp"
 ssh hadoop@slave08.hadoop "mkdir -p /data/hadoop/cluster01/dfs/name"
 ssh hadoop@slave08.hadoop "mkdir -p /data/hadoop/cluster01/dfs/data"
