1c175f29851db4fac5056b894d793c5ecc005d9c
fsmflying <fangmingdev@163.com>
2018-08-11 16:44:26 +0800

    修改脚本：数据目录重建

--- hadoop-2.7.7/cluster/cluster01/recreate-data-dir-all.sh
+++ hadoop-2.7.7/cluster/cluster01/recreate-data-dir-all.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
 #清除数据目录
-rm -rf /data/hadoop/cluster01
-ssh hadoop@slave06.hadoop "rm -rf /data/hadoop/cluster01"
-ssh hadoop@slave07.hadoop "rm -rf /data/hadoop/cluster01"
 
-mkdir -p /data/hadoop/cluster01/tmp
-mkdir -p /data/hadoop/cluster01/dfs/name
-mkdir -p /data/hadoop/cluster01/dfs/data
+ssh hadoop@master.hadoop "mkdir -p /data/hadoop/cluster01/tmp"
+ssh hadoop@master.hadoop "mkdir -p /data/hadoop/cluster01/dfs/name"
+ssh hadoop@master.hadoop "mkdir -p /data/hadoop/cluster01/dfs/data"
 
 ssh hadoop@slave06.hadoop "mkdir -p /data/hadoop/cluster01/tmp"
 ssh hadoop@slave06.hadoop "mkdir -p /data/hadoop/cluster01/dfs/name"
