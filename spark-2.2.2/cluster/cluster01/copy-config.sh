8ca4980ba2fba60eafeddafa11d944cc58358e2b
fsmflying <fangmingdev@163.com>
2018-08-11 22:23:03 +0800

    spark中添加copy-config.sh,copy-config-from-local.sh

---
+++ spark-2.2.2/cluster/cluster01/copy-config.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+
+ssh hadoop@slave06.hadoop "rm -rf /usr/local/spark/sbin"
+scp -R hadoop@master.hadoop:/usr/local/spark/sbin hadoop@slave06.hadoop:/usr/local/spark/
+ssh hadoop@slave06.hadoop "rm -rf /usr/local/spark/bin"
+scp -R hadoop@master.hadoop:/usr/local/spark/bin hadoop@slave06.hadoop:/usr/local/spark/
+ssh hadoop@slave06.hadoop "rm -rf /usr/local/spark/conf"
+scp -R hadoop@master.hadoop:/usr/local/spark/conf hadoop@slave06.hadoop:/usr/local/spark/
+
+
+ssh hadoop@slave07.hadoop "rm -rf /usr/local/spark/sbin"
+scp -R hadoop@master.hadoop:/usr/local/spark/sbin hadoop@slave07.hadoop:/usr/local/spark/
+ssh hadoop@slave07.hadoop "rm -rf /usr/local/spark/bin"
+scp -R hadoop@master.hadoop:/usr/local/spark/bin hadoop@slave07.hadoop:/usr/local/spark/
+ssh hadoop@slave07.hadoop "rm -rf /usr/local/spark/conf"
+scp -R hadoop@master.hadoop:/usr/local/spark/conf hadoop@slave07.hadoop:/usr/local/spark/
+
+ssh hadoop@slave08.hadoop "rm -rf /usr/local/spark/sbin"
+scp -R hadoop@master.hadoop:/usr/local/spark/sbin hadoop@slave08.hadoop:/usr/local/spark/
+ssh hadoop@slave08.hadoop "rm -rf /usr/local/spark/bin"
+scp -R hadoop@master.hadoop:/usr/local/spark/bin hadoop@slave08.hadoop:/usr/local/spark/
+ssh hadoop@slave08.hadoop "rm -rf /usr/local/spark/conf"
+scp -R hadoop@master.hadoop:/usr/local/spark/conf hadoop@slave08.hadoop:/usr/local/spark/
