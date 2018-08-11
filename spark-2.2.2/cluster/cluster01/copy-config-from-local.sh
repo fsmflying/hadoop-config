e50dc9878a7905a655af56c4cc153dc69a22aefb
fsmflying <fangmingdev@163.com>
2018-08-11 22:37:36 +0800

    copy-config.sh中scp -R改为scp -r

--- spark-2.2.2/cluster/cluster01/copy-config-from-local.sh
+++ spark-2.2.2/cluster/cluster01/copy-config-from-local.sh
@@ -2,23 +2,23 @@
 
 
 ssh hadoop@slave06.hadoop "rm -rf /usr/local/spark/sbin"
-scp -R /usr/local/spark/sbin hadoop@slave06.hadoop:/usr/local/spark/
+scp -r /usr/local/spark/sbin hadoop@slave06.hadoop:/usr/local/spark/
 ssh hadoop@slave06.hadoop "rm -rf /usr/local/spark/bin"
-scp -R /usr/local/spark/bin hadoop@slave06.hadoop:/usr/local/spark/
+scp -r /usr/local/spark/bin hadoop@slave06.hadoop:/usr/local/spark/
 ssh hadoop@slave06.hadoop "rm -rf /usr/local/spark/conf"
-scp -R /usr/local/spark/conf hadoop@slave06.hadoop:/usr/local/spark/
+scp -r /usr/local/spark/conf hadoop@slave06.hadoop:/usr/local/spark/
 
 
 ssh hadoop@slave07.hadoop "rm -rf /usr/local/spark/sbin"
-scp -R /usr/local/spark/sbin hadoop@slave07.hadoop:/usr/local/spark/
+scp -r /usr/local/spark/sbin hadoop@slave07.hadoop:/usr/local/spark/
 ssh hadoop@slave07.hadoop "rm -rf /usr/local/spark/bin"
-scp -R /usr/local/spark/bin hadoop@slave07.hadoop:/usr/local/spark/
+scp -r /usr/local/spark/bin hadoop@slave07.hadoop:/usr/local/spark/
 ssh hadoop@slave07.hadoop "rm -rf /usr/local/spark/conf"
-scp -R /usr/local/spark/conf hadoop@slave07.hadoop:/usr/local/spark/
+scp -r /usr/local/spark/conf hadoop@slave07.hadoop:/usr/local/spark/
 
 ssh hadoop@slave08.hadoop "rm -rf /usr/local/spark/sbin"
-scp -R /usr/local/spark/sbin hadoop@slave08.hadoop:/usr/local/spark/
+scp -r /usr/local/spark/sbin hadoop@slave08.hadoop:/usr/local/spark/
 ssh hadoop@slave08.hadoop "rm -rf /usr/local/spark/bin"
-scp -R /usr/local/spark/bin hadoop@slave08.hadoop:/usr/local/spark/
+scp -r /usr/local/spark/bin hadoop@slave08.hadoop:/usr/local/spark/
 ssh hadoop@slave08.hadoop "rm -rf /usr/local/spark/conf"
-scp -R /usr/local/spark/conf hadoop@slave08.hadoop:/usr/local/spark/
+scp -r /usr/local/spark/conf hadoop@slave08.hadoop:/usr/local/spark/
